c998d3ef9ac3a278b5fa0db442d0d42fa3c84d30
svhanz <serg-69@mail.ru>
2021-10-11 21:24:11 +0300

    master

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>YCDL</title>
+  <meta name="description" content="david lin film director and his films." />
+  <meta name="keywords"
+    content="david lin,lin david,ycdl,director,imagefilm,image film,music video,musicvideo,musik video,musikvideo,creative producer,promotional video,promotional film,commercial video,commercial film" />
+  <!--<link rel="stylesheet" href="css/normalize.css" /> -->
+  <link rel="stylesheet" href="css/swiper-bundle.min.css" />
+  <link rel="stylesheet" href="css/style.css" />
+</head>
+
+<body>
+  <!--==START HEADER==-->
+  <header>
+    <div class="headerInside">
+      <div class="logo">
+        <a href="#">
+          <img class="logo" src="img/ycdl-logo.svg" alt="Logo YCDL" />
+        </a>
+      </div>
+      <div class="navWrap">
+        <a href="#">films</a>
+        <a href="#">contact</a>
+        <a href="#">about</a>
+      </div>
+    </div>
+  </header>
+  <!--==END HEADER==-->
+
+  <!--==MAIN CONTENT==-->
+  <main>
+    <!--==START SWIPER==-->
+    <div class="swiper">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide">
+          <img src="img/27horowitz2-jumbo-v2.jpg" alt="" />
+        </div>
+        <div class="swiper-slide">
+          <img src="img/dog_terrier_mix_272830.jpg" alt="" />
+        </div>
+        <div class="swiper-slide">
+          <img src="img/dogs_dog_animal_215598.jpg" alt="" />
+        </div>
+      </div>
+      <!--==Add Pagination==-->
+      <div class="swiper-pagination"></div>
+      <!-- Add Arrows -->
+      <div class="swiper-button-next"></div>
+      <div class="swiper-button-prev"></div>
+    </div>
+    <!--==END SWIPER==-->
+  </main>
+  <!--==END MAIN==-->
+
+  <!--==START FOOTER==-->
+  <footer>
+    <div class="footerContainer">
+      <p class="footerContainer-text">© 2021 david lin. all rights reserved.</p>
+    </div>
+  </footer>
+  <!--==END FOOTER==-->
+  <script src="js/swiper-bundle.min.js"></script>
+  <!--<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>-->
+  <script src="js/script.js"></script>
+</body>
+
+</html>
